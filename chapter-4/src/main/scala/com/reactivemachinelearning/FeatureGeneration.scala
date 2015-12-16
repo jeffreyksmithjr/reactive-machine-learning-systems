c6c1ca539292e03e8082c616894175b4baa9dfc4
Jeff Smith <jeffreyksmithjr@gmail.com>
2015-12-15 22:04:28 -0500

    updating usage of squawks

--- chapter-4/src/main/scala/com/reactivemachinelearning/FeatureGeneration.scala
+++ chapter-4/src/main/scala/com/reactivemachinelearning/FeatureGeneration.scala
@@ -11,15 +11,17 @@ object FeatureGeneration extends App {
   val sc = new SparkContext(conf)
   val sqlContext = new SQLContext(sc)
 
+  case class Squawk(id: Int, text: String)
+
   // Input data: Each row is a 140 character or less squawk
-  val documentDF = sqlContext.createDataFrame(Seq(
-    (0, "Clouds sure make it hard to look on the bright side of things."),
-    (1, "Who really cares who gets the worm?  I'm fine with sleeping in."),
-    (2, "Why don't french fries grow on trees?"))).toDF("label", "squawk")
+  val squawks = sqlContext.createDataFrame(Seq(
+    Squawk(123, "Clouds sure make it hard to look on the bright side of things."),
+    Squawk(124, "Who really cares who gets the worm?  I'm fine with sleeping in."),
+    Squawk(125, "Why don't french fries grow on trees?"))).toDF("label", "squawk")
 
   val tokenizer = new Tokenizer().setInputCol("squawk").setOutputCol("words")
 
-  val tokenized = tokenizer.transform(documentDF)
+  val tokenized = tokenizer.transform(squawks)
   tokenized.select("words", "label").take(3).foreach(println)
 
 }
