657703606a629ade5f09c66630808da92c2a4d7c
Jeff Smith <jeffreyksmithjr@gmail.com>
2015-12-19 17:38:50 -0500

    transformation example, refactoring

--- chapter-4/src/main/scala/com/reactivemachinelearning/FeatureGeneration.scala
+++ chapter-4/src/main/scala/com/reactivemachinelearning/FeatureGeneration.scala
@@ -1,6 +1,6 @@
 package com.reactivemachinelearning
 
-import org.apache.spark.ml.feature.Tokenizer
+import org.apache.spark.ml.feature.{HashingTF, Tokenizer}
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -14,14 +14,53 @@ object FeatureGeneration extends App {
   case class Squawk(id: Int, text: String)
 
   // Input data: Each row is a 140 character or less squawk
-  val squawks = sqlContext.createDataFrame(Seq(
-    Squawk(123, "Clouds sure make it hard to look on the bright side of things."),
+  val squawks = Seq(Squawk(123, "Clouds sure make it hard to look on the bright side of things."),
     Squawk(124, "Who really cares who gets the worm?  I'm fine with sleeping in."),
-    Squawk(125, "Why don't french fries grow on trees?"))).toDF("label", "squawk")
+    Squawk(125, "Why don't french fries grow on trees?"))
+
+  val squawksDF = sqlContext.createDataFrame(squawks).toDF("squawkId", "squawk")
 
   val tokenizer = new Tokenizer().setInputCol("squawk").setOutputCol("words")
 
-  val tokenized = tokenizer.transform(squawks)
-  tokenized.select("words", "label").take(3).foreach(println)
+  val tokenized = tokenizer.transform(squawksDF)
+  tokenized.select("words", "squawkId").foreach(println)
+
+  trait Feature {
+    val name: String
+    val value: Any
+  }
+
+  case class WordSequenceFeature(name: String, value: Seq[String]) extends Feature
+
+  val wordsFeatures = tokenized.select("words")
+    .map[WordSequenceFeature](row =>
+    WordSequenceFeature("words",
+      row.getSeq[String](0)))
+
+  wordsFeatures.foreach(println)
+
+  val hashingTF = new HashingTF()
+    .setInputCol("words")
+    .setOutputCol("termFrequencies")
+
+  val tfs = hashingTF.transform(tokenized)
+
+  tfs.select("termFrequencies").foreach(println)
+
+  case class IntFeature(name: String, value: Int) extends Feature
+
+  case class BinaryFeature(name: String, value: Boolean) extends Feature
+
+  def binarize(feature: IntFeature, threshold: Double): BinaryFeature = {
+    BinaryFeature(feature.name, feature.value > threshold)
+  }
+
+  val SUPER_THRESHOLD = 1000000
+
+  val squirrelFollowers = IntFeature("followers", 12)
+  val slothFollowers = IntFeature("followers", 23584166)
+
+  val squirrelIsSuper = binarize(squirrelFollowers, SUPER_THRESHOLD)
+  val slothIsSuper = binarize(slothFollowers, SUPER_THRESHOLD)
 
 }
