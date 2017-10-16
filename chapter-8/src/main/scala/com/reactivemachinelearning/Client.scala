c17222055a839cdc77bfc6cccc9ff345b0ff21aa
Jeff Smith <jeffreyksmithjr@gmail.com>
2017-10-16 10:57:54 -0400

    adding return signature

--- chapter-8/src/main/scala/com/reactivemachinelearning/Client.scala
+++ chapter-8/src/main/scala/com/reactivemachinelearning/Client.scala
@@ -1,18 +1,20 @@
 package com.reactivemachinelearning
 
+import org.http4s.Response
 import org.http4s.Uri
 import org.http4s.client.blaze.PooledHttp1Client
 
 import scala.concurrent.duration._
+import scalaz.concurrent.Task
 
 object Client {
 
   val client = PooledHttp1Client()
 
-  private def call(model: String, input: String) = {
+  private def call(model: String, input: String): Task[Response] = {
     val target = Uri.fromString(s"http://localhost:8080/models/$model/$input").toOption.get
-//    client.expect[String](target)
-    client(target).retry(Seq(1 second), {_ => true})
+    //    client.expect[String](target)
+    client(target).retry(Seq(1 second), { _ => true })
   }
 
   def callA(input: String) = call("a", input)
